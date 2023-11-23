269784f015ce61f205d9044242f7466749ce053f
윤형석 <104922729+pledge24@users.noreply.github.com>
2023-11-23 22:26:17 +0900

    version 1.5 테스트 버전 업데이트
    태담 작업(MelonV4_1: UI 디자인 및 추가)과 형석 작업(MelonV4_2: 캐릭터 및 기능성 버튼 추가) MelonV5_test 씬에 통합완료.
    
    업데이트 내용
    - 터치스크린식 카메라 줌인 / 줌아웃 기능
    - 카메라 회전 기능
    - 캐릭터 5명으로 늘림(전부 리깅 되어있음)
    - 다양한 버튼 추가
    
    - UI 추가 및 애니메이션 적용
    - 음악 추가
    - 디스플레이 변경
    - 사진으로 배경 추가

--- This_Is_My_Capstone/Assets/VRM_Models/taedamTest/camcam1.cs
+++ This_Is_My_Capstone/Assets/VRM_Models/taedamTest/camcam1.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class camcam1 : MonoBehaviour
 {
-    [SerializeField] private float left = 0.0f;
-    [SerializeField] private float right = 1.0f;
-    [SerializeField] private float bottom = 0.0f;
-    [SerializeField] private float top = 1.0f;
-
     private Camera mainCamera;
     private bool flagcam = true;
     private float initialDistance; // 초기 손가락 간 거리
-    private float zoomSpeed = 0.01f; // 조정 가능한 줌 속도
+    private float zoomSpeed = 0.001f; // 조정 가능한 줌 속도
+    private float rotateSpeed = 0.002f; // 조정 가능한 줌 속도
     private Vector2 initialTouchPosition; // 초기 터치 위치
 
     private float minFOV = -2f;
     private float maxFOV = 0.35f;
+
+    [SerializeField] private GameObject stage;
+
+    private float xRotateMove, yRotateMove;
 
     void Update()
     {
@@ -25,6 +25,7 @@ public class camcam1 : MonoBehaviour
             // 두 손가락 간 거리 계산
             Touch touch1 = Input.GetTouch(0);
             Touch touch2 = Input.GetTouch(1);
+
             float currentDistance = Vector2.Distance(touch1.position, touch2.position);
 
             if (touch1.phase == TouchPhase.Began || touch2.phase == TouchPhase.Began)
@@ -34,8 +35,10 @@ public class camcam1 : MonoBehaviour
             else if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved)
             {
                 // 손가락 간 거리 변화에 따라 카메라 줌 조절
-                float deltaDistance = initialDistance - currentDistance ;
+                float deltaDistance = currentDistance - initialDistance;
+                //Debug.Log(string.Format("deltaDistance {0:F2}", deltaDistance));
                 Zoom(deltaDistance);
+                initialDistance = currentDistance;
             }
         }
         if (Input.touchCount == 1)
@@ -51,7 +54,11 @@ public class camcam1 : MonoBehaviour
                 // 손가락의 이동량에 따라 카메라 회전
                 float deltaX = touch.position.x - initialTouchPosition.x;
                 float deltaY = touch.position.y - initialTouchPosition.y;
-                RotateCamera(deltaX, deltaY);
+
+                //Debug.Log(string.Format("s_pos: {0:F2}, deltaX: {1:F2} deltaY {2:F2}", stage.transform.position, deltaX, deltaY));
+
+                transform.RotateAround(stage.transform.position, Vector3.up, deltaX * rotateSpeed);
+                transform.LookAt(stage.transform.position);
             }
         }
     }
@@ -60,13 +67,18 @@ public class camcam1 : MonoBehaviour
     {
         mainCamera = GetComponent<Camera>();
     }
+
     void Zoom(float delta)
     {
-        //mainCamera.transform.position.z
-        mainCamera.fieldOfView += delta * zoomSpeed;
+        float z_pos = mainCamera.transform.position.z + delta * zoomSpeed;
+        
         // 필요한 경우 최소/최대 제한을 추가할 수 있습니다.
-        if (Camera.main.fieldOfView < minFOV) Camera.main.fieldOfView = minFOV;
-        if (Camera.main.fieldOfView > maxFOV) Camera.main.fieldOfView = maxFOV;
+        
+        if (minFOV < z_pos && z_pos < maxFOV)
+        {
+            mainCamera.transform.Translate(0, 0, delta * zoomSpeed);
+        }
+        
     }
     void RotateCamera(float x, float y)
     {

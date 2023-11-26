b33859b9453d1a4228b5f3de6bbc7498dcd8723c
윤형석 <104922729+pledge24@users.noreply.github.com>
2023-11-26 23:21:08 +0900

    배경화면 추가
    총 19개의 배경을 추가하였습니다.
    - 13개의 CubeMap
    - 6개의 Asset, animation이 포함된 맵
    
    배경은 기타 설정 탭에서 변경할 수 있는 버튼을 추가하였습니다.
    
    !주의) 이번 커밋은 용량이 큽니다.

---
+++ This_Is_My_Capstone/Assets/BACKGROUD/CUBEMAP/bg_script/swithBackGround.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class swithBackGround : MonoBehaviour
+{
+
+    [SerializeField] GameObject cubeMap;
+    [SerializeField] Text map_name;
+    [SerializeField] List<Material> materials;
+    [SerializeField] List<GameObject> Assets;
+
+    private int bg_id = 0;
+    private int prev_id = 0;
+
+    private int noAssetBG = 13;
+
+    MeshRenderer cubeMap_renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeMap_renderer = cubeMap.GetComponent<MeshRenderer>(); Debug.Log(materials.Count);
+       
+    }
+
+    private void OnOffAssets()
+    {
+        for(int i = 0; i< Assets.Count; i++)
+        {
+            Assets[i].SetActive(false);
+        }
+
+        if(bg_id >= noAssetBG)
+        {
+            Assets[bg_id - noAssetBG].SetActive(true);
+        }
+    }
+
+    public void switchBG_left()
+    {
+        prev_id = bg_id;
+
+        bg_id = (bg_id - 1) >= 0 ? bg_id - 1 : materials.Count - 1;
+        cubeMap_renderer.material = materials[bg_id];
+        map_name.text = materials[bg_id].name;
+        Debug.Log(string.Format("material name: {0}",materials[bg_id].name));
+
+        OnOffAssets();
+    }
+
+    public void switchBG_right()
+    {
+        prev_id = bg_id;
+
+        bg_id = (bg_id + 1) % materials.Count;
+        cubeMap_renderer.material = materials[bg_id];
+        map_name.text = materials[bg_id].name;
+        Debug.Log(string.Format("material name: {0}", materials[bg_id].name));
+
+        OnOffAssets();
+    }
+}
